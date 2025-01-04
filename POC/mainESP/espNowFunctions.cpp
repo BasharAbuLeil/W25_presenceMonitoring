175e393068c23719a1c9d7b8b201657ed0fd7842
BasharAbuLeil <bashar.ab@campus.technion.ac.il>
2025-01-03 22:48:29 -0800

    esp now sender implementation

--- POC/mainESP/espNowFunctions.cpp
+++ POC/mainESP/espNowFunctions.cpp
@@ -1,3 +1,42 @@
 #include "espNowFunctions.h"
 
+uint8_t peerAddress[]={0x10, 0x06, 0x1C, 0x86, 0x1D, 0xB0};
 
+espNow::espNow():m_isOngoingSession(false),m_macAddress(peerAddress){
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    throw espNowExceptions();
+  }
+  // esp_now_register_recv_cb(OnDataRecv);
+  // esp_now_register_send_cb(OnDataSent);
+  memcpy(m_peerInfo.peer_addr, m_macAddress, 6);
+  m_peerInfo.channel = 0;
+  m_peerInfo.encrypt = false;
+  if (esp_now_add_peer(&m_peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
+}
+
+void espNow::initSession(){
+  msg m1;
+  m1.init=true;
+  esp_now_send(peerAddress, (uint8_t *)&m1, sizeof(m1));
+}
+
+
+
+void espNow::haltSession(){
+  msg m1;
+  m1.init=false;
+  esp_now_send(peerAddress, (uint8_t *)&m1, sizeof(m1));
+}
+
+
+
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status){
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
--- POC/mainESP/mainESP.ino
+++ POC/mainESP/mainESP.ino
@@ -4,21 +4,17 @@
 
 
 bool onGoingSession=false;
-
+espNow* peerCommunicator;
 
 void setup() {
   // put your setup code here, to run once:
   //boot sequnce if important. screen -> sd -> wifi -> fireStore.
-  espNow peerCommunicator=new espNow;
+  peerCommunicator=new espNow;
 }
 
 void loop() {
-  if(!onGoingSesson){
-    //output get id message on the screen .
-    // key pad aware. 
-  }
-  else {
-    //manage esp now  communication
-    // 
-  }
+  peerCommunicator->initSession();
+  delay(10000);
+  peerCommunicator->haltSession();
+  delay(5000);
 }
