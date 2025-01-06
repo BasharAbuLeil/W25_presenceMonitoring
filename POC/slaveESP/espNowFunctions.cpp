b4825640ba9bf415533e8b85175d4027e1acf95b
BasharAbuLeil <bashar.ab@campus.technion.ac.il>
2025-01-06 00:38:23 -0800

    Built in indicator added to the slave

--- POC/slaveESP/espNowFunctions.cpp
+++ POC/slaveESP/espNowFunctions.cpp
@@ -1,46 +1,46 @@
-#include "espNowFunctions.h"
+// #include "espNowFunctions.h"
 
-// Define a peer address
-uint8_t peerAddress[] = {0x10, 0x06, 0x1C, 0x86, 0xA2, 0x9C};
+// // Define a peer address
+// uint8_t peerAddress[] = {0x10, 0x06, 0x1C, 0x86, 0xA2, 0x9C};
 
-// Constructor for espNow class
-espNow::espNow() : m_isOngoingSession(false), m_macAddress(peerAddress) {
-  WiFi.mode(WIFI_STA);
+// // Constructor for espNow class
+// espNow::espNow() : m_isOngoingSession(false), m_macAddress(peerAddress) {
+//   WiFi.mode(WIFI_STA);
 
-  if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
-    throw espNowExceptions();
-  }
+//   if (esp_now_init() != ESP_OK) {
+//     Serial.println("Error initializing ESP-NOW");
+//     throw espNowExceptions();
+//   }
 
-  esp_now_register_recv_cb(OnDataRecv);
-  esp_now_register_send_cb(OnDataSent);
+//   esp_now_register_recv_cb(OnDataRecv);
+//   esp_now_register_send_cb(OnDataSent);
 
-  memcpy(m_peerInfo.peer_addr, m_macAddress, 6);
-  m_peerInfo.channel = 0;
-  m_peerInfo.encrypt = false;
+//   memcpy(m_peerInfo.peer_addr, m_macAddress, 6);
+//   m_peerInfo.channel = 0;
+//   m_peerInfo.encrypt = false;
 
-  if (esp_now_add_peer(&m_peerInfo) != ESP_OK) {
-    Serial.println("Failed to add peer");
-    return;
-  }
-}
+//   if (esp_now_add_peer(&m_peerInfo) != ESP_OK) {
+//     Serial.println("Failed to add peer");
+//     return;
+//   }
+// }
 
-// Callback function executed when data is sent
-void espNow::OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
+// // Callback function executed when data is sent
+// void espNow::OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+//   Serial.print("\r\nLast Packet Send Status:\t");
+//   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+// }
 
-// Callback function executed when data is received
-void espNow::OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
-  recievedMsg m1;
-  memcpy(&m1, incomingData, sizeof(m1));
-  Serial.println("Data received");
-  Serial.print("Session status: ");
-  Serial.println(m1.init);
-}
+// // Callback function executed when data is received
+// void espNow::OnDataRecv(const esp_now_recv_info *recv_info, const uint8_t *incomingData, int len) {
+//   recievedMsg m1;
+//   memcpy(&m1, incomingData, sizeof(m1));
+//   Serial.println("Data received");
+//   Serial.print("Session status: ");
+//   Serial.println(m1.init);
+// }
 
-// Method to check ongoing session status
-bool espNow::isOngoingSession() const {
-  return m_isOngoingSession;
-}
+// // Method to check ongoing session status
+// bool espNow::isOngoingSession() const {
+//   return m_isOngoingSession;
+// }
--- POC/slaveESP/slaveESP.ino
+++ POC/slaveESP/slaveESP.ino
@@ -1,6 +1,9 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
+
+
+#define BUILT_IN_LED 2
 // Define a data structure for receiving session control
 typedef struct struct_session {
     bool d;
@@ -14,8 +17,9 @@ typedef struct struct_data {
 // Create structured objects
 struct_session sessionData;
 struct_data sendData;
-const char* ssid = "Bashar";
-const char* password = "toz12345";
+const char* ssid = "";
+const char* password = "";
+esp_now_peer_info_t peerInfo;
 // Global variables
 bool session = false;
 int value = 0;
@@ -39,56 +43,68 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 }
 
 void setup() {
-    // Set up Serial Monitor
-    Serial.begin(115200);
-    
-    // Set ESP32 as a Wi-Fi Station
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    // Initialize ESP-NOW
-    if (esp_now_init() != ESP_OK) {
-        Serial.println("Error initializing ESP-NOW");
-        return;
-    }
-    Serial.println("ESP-NOW initialized.");
-    
-    // Register callback functions
-    esp_now_register_recv_cb(onDataReceive);
-    esp_now_register_send_cb(OnDataSent);
-    
-    // Add the master as a peer
-    esp_now_peer_info_t peerInfo;
-    memcpy(peerInfo.peer_addr, masterMACAddress, 6);
-    peerInfo.channel = WiFi.channel(); // Use the same channel as the master
-    peerInfo.encrypt = false;  // No encryption used (modify if you want encryption)
-
-    delay(100);
-    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-        Serial.println("Failed to add peer");
-        return;
-    }
-    Serial.println("Peer added.");
+  
+  // Set up Serial Monitor
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  int tries=0;
+  //while(WiFi.status() != WL_CONNECTED) {
+  //   tries++;
+  //   delay(1000);
+  //   Serial.print(".");
+  // }
+  Serial.println("ssid:");
+  Serial.println(ssid);
+  Serial.println("password");
+  Serial.println(password);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(onDataReceive);
+  memcpy(peerInfo.peer_addr, masterMACAddress, 6);
+  peerInfo.channel = WiFi.channel();  
+  peerInfo.encrypt = false;
+  
+  // Add peer  
+  delay(100);      
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+  pinMode(BUILT_IN_LED, OUTPUT);
+  //ending the esp now part 
+  //wifi part
+  
+  
+  
+  // Register peer
+  
 }
 
 void loop() {
     if(session) {
-        Serial.print("value == ");
-        Serial.println(value);
+      digitalWrite(BUILT_IN_LED, HIGH);
+      Serial.print("value == ");
+      Serial.println(value);
         
-        // Increment value and prepare data to send
-        value++;
-        sendData.value = value;
+      // Increment value and prepare data to send
+      value++;
+      sendData.value = value;
         
-        // Send data to master
-        esp_err_t result = esp_now_send(masterMACAddress, (uint8_t *)&sendData, sizeof(sendData));
+      // Send data to master
+      esp_err_t result = esp_now_send(masterMACAddress, (uint8_t *)&sendData, sizeof(sendData));
         
-        if (result == ESP_OK) {
-            Serial.println("Sending confirmed");
-        } else {
-            Serial.println("Sending error");
-        }
+      if (result == ESP_OK) {
+           Serial.println("Sending confirmed");
+      }else {
+           Serial.println("Sending error");
+      }
     } else {
-        value = 0;
+      digitalWrite(BUILT_IN_LED, LOW);
+      value = 0;
     }
     //test
     delay(1000);
